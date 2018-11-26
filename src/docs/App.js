e3ed41068f7fe1057f2b8833b097b68171151e6e
Nursultan <nberkimbayev@gmail.com>
2018-11-26 14:10:24 +0600

    feat(chore): Release v0.3.0

--- src/docs/App.js
+++ src/docs/App.js
@@ -8,7 +8,6 @@ export default function () {
   return (
     <>
       <Helmet>
-        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>Charm</title>
