abd6c9d4c8d176436036c960be4bd8d22652ae37
Nursultan <nberkimbayev@gmail.com>
2018-11-26 19:15:33 +0600

    feat(docs): Add docs shell component

--- src/docs/App.js
+++ src/docs/App.js
@@ -1,8 +1,9 @@
-import React, { memo } from 'react';
-import { Helmet } from 'react-helmet';
+import { memo } from 'react';
 
 import Main from './mdx/main.mdx';
 import { Heading } from '../components/index';
+
+import Shell from './Shell';
 
 const rewriteComponents = {
   h1: memo(props => (
@@ -15,22 +16,8 @@ const rewriteComponents = {
 
 export default function App() {
   return (
-    <>
-      <Helmet>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <title>Charm</title>
-      </Helmet>
-
-      <div className="layout">
-        <nav className="nav">
-          <ul className="list list--inline list--style-none">
-            <li className="text--secondary"><small className="small">v16.7.0</small></li>
-            <li className="text--secondary"><small className="small">Getting Started</small></li>
-            <li className="text--light"><small className="small">20th November</small></li>
-          </ul>
-        </nav>
-
+    <Shell>
+      <>
         <div className="content">
           <Main
             components={rewriteComponents}
@@ -54,31 +41,7 @@ export default function App() {
             </button>
           </div>
         </div>
-      </div>
-
-      <div className="aside">
-        <div className="aside__logo">
-          <small className="small text--bold">React Library</small>
-        </div>
-
-        <ul className="list--aside">
-          <li className="list-item--style-none"><a href="" className="link link_main is-active">Getting started</a></li>
-          <li className="list-item--style-none">
-            <ul className="list--aside margin-bottom--1">
-              <li><a href="" className="link link_main link_small">Try React</a></li>
-              <li className="list-item--style-none">
-                <ul className="list--aside">
-                  <li><a href="" className="link link_main link_small">More on react</a></li>
-                  <li><a href="" className="link link_main link_small">Work with react</a></li>
-                  <li><a href="" className="link link_main link_small">Contributions</a></li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li className="list-item--style-none"><a href="" className="link link_main">Contributions</a></li>
-          <li className="list-item--style-none"><a href="" className="link link_main">GitHub</a></li>
-        </ul>
-      </div>
-    </>
+      </>
+    </Shell>
   );
 }
---
+++ src/docs/Shell.js
@@ -0,0 +1,49 @@
+import { Helmet } from 'react-helmet';
+
+export default function App(props) {
+  return (
+    <>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <title>Charm</title>
+      </Helmet>
+
+      <div className="layout">
+        {props.children}
+      </div>
+
+      <nav className="nav">
+        <ul className="list list--inline list--style-none">
+          <li className="text--secondary"><small className="small">v16.7.0</small></li>
+          <li className="text--secondary"><small className="small">Getting Started</small></li>
+          <li className="text--light"><small className="small">20th November</small></li>
+        </ul>
+      </nav>
+
+      <div className="aside">
+        <div className="aside__logo">
+          <small className="small text--bold">React Library</small>
+        </div>
+
+        <ul className="list--aside">
+          <li className="list-item--style-none"><a href="" className="link link_main is-active">Getting started</a></li>
+          <li className="list-item--style-none">
+            <ul className="list--aside margin-bottom--1">
+              <li><a href="" className="link link_main link_small">Try React</a></li>
+              <li className="list-item--style-none">
+                <ul className="list--aside">
+                  <li><a href="" className="link link_main link_small">More on react</a></li>
+                  <li><a href="" className="link link_main link_small">Work with react</a></li>
+                  <li><a href="" className="link link_main link_small">Contributions</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li className="list-item--style-none"><a href="" className="link link_main">Contributions</a></li>
+          <li className="list-item--style-none"><a href="" className="link link_main">GitHub</a></li>
+        </ul>
+      </div>
+    </>
+  );
+}
