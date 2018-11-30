aef89df288ac266a6c4164d651e8e5ffb35ad9fa
Nursultan <nberkimbayev@gmail.com>
2018-11-30 12:11:58 +0600

    fix(propTypes): Fix proptypes extension

--- src/proptypes/layout.js
+++ src/proptypes/layout.js
@@ -2,41 +2,50 @@ import PropTypes from 'prop-types';
 
 import { ContentProps, RouteProps } from './content';
 
-export const FooterProps = {
+const FooterProps = PropTypes.shape({
   prevRoute: RouteProps,
   nextRoute: RouteProps,
   onClickHandler: PropTypes.func,
-};
+});
 
-export const ListProps = {
+const ListProps = PropTypes.shape({
   listContents: ContentProps,
   isInner: PropTypes.bool,
   LinkComponent: PropTypes.element,
-};
+});
 
-export const AsideProps = {
+const AsideProps = PropTypes.shape({
   logo: PropTypes.oneOfType([
     PropTypes.element,
     PropTypes.string,
   ]),
   list: PropTypes.element,
   listProps: ListProps,
-};
+});
 
-export const NavProps = {
+const NavProps = PropTypes.shape({
   title: PropTypes.string,
   lastUpdate: PropTypes.string,
   version: PropTypes.string,
-};
+});
 
-export const LayoutProps = {
+const LayoutProps = {
   children: PropTypes.node,
+
   footer: PropTypes.element,
   aside: PropTypes.element,
   nav: PropTypes.element,
-  // componentProps
+
   listProps: ListProps,
   asideProps: AsideProps,
   navProps: NavProps,
   footerProps: FooterProps,
+};
+
+export {
+  LayoutProps,
+  FooterProps,
+  NavProps,
+  AsideProps,
+  ListProps,
 };
