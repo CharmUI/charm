fbc719f9fee3e36766120961b06b93fdb8b61ebf
Nursultan <nberkimbayev@gmail.com>
2018-12-02 15:44:44 +0600

    feat(footer): Refactor

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -9,42 +9,48 @@ function Footer({
 }) {
   return (
     <footer className="footer">
-      { prevRoute && (
-        <button
-          className="button"
-          type="button"
-          onClick={() => onFooterLinkClick(prevRoute.path)}
-        >
-          Previous
-          <br />
-          <span className="text--blue">
-            —&nbsp;
-            { prevRoute.name }
-          </span>
-        </button>
-      ) }
+      { prevRoute
+        ? (
+          <button
+            className="button"
+            type="button"
+            onClick={() => onFooterLinkClick(prevRoute.path)}
+          >
+            Previous
+            <br />
+            <span className="text--blue">
+              —&nbsp;
+              { prevRoute.name }
+            </span>
+          </button>
+        )
+        : <div />
+      }
 
-      { nextRoute && (
-        <button
-          className="button text--right"
-          type="button"
-          onClick={() => onFooterLinkClick(nextRoute.path)}
-        >
-          Next
-          <br />
-          <span className="text--blue">
-            { nextRoute.name }
-            &nbsp;—
-          </span>
-        </button>
-      ) }
+      { nextRoute
+        ? (
+          <button
+            className="button text--right"
+            type="button"
+            onClick={() => onFooterLinkClick(nextRoute.path)}
+          >
+            Next
+            <br />
+            <span className="text--blue">
+              { nextRoute.name }
+              &nbsp;—
+            </span>
+          </button>
+        )
+        : <div />
+      }
     </footer>
   );
 }
 
 Footer.defaultProps = {
-  prevRoute: (<div />),
-  nextRoute: (<div />),
+  prevRoute: null,
+  nextRoute: null,
   onFooterLinkClick: () => {},
 };
 
