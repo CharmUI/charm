3176aaecbcb6ab11ad17ff142ac3f1afc0652c1d
Nursultan <nberkimbayev@gmail.com>
2018-12-13 22:07:11 +0600

    feat(ui-nav): Refactor nav component

--- src/layout/nav.js
+++ src/layout/nav.js
@@ -10,18 +10,7 @@ function Nav({
       <ul className="list list--inline list--style-none">
         { content && content.map((item, index) => (
           <li key={index} className="text--secondary">
-            { item.type && item.type === 'button'
-              ? (
-                <button
-                  type="button"
-                  className={`button text--small ${item.className}`}
-                  onClick={item.onClick}
-                >
-                  <small className="small">{ item.name }</small>
-                </button>
-              )
-              : (<small className="small">{ item.name }</small>)
-            }
+            { item }
           </li>
         )) }
       </ul>
